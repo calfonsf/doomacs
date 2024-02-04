f811ce9d1c1411ce27c71f5501ab228cc7ab3b04
calfonsf <calfonsf@gmail.com>
2024-02-04 17:00:20 -0500

    added configs

---
+++ custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1a1b26" "#f7768e" "#73daca" "#e0af68" "#7aa2f7" "#bb9af7" "#b4f9f8" "#a9b1d6"])
+ '(custom-safe-themes
+   '("56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "3de5c795291a145452aeb961b1151e63ef1cb9565e3cdbd10521582b5fd02e9a" default))
+ '(exwm-floating-border-color "#252630")
+ '(fci-rule-color "#9099c0")
+ '(highlight-tail-colors ((("#222e36") . 0) (("#29313b") . 20)))
+ '(ispell-dictionary nil)
+ '(jdee-db-active-breakpoint-face-colors (cons "#414868" "#b4f9f8"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#414868" "#73daca"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#414868" "#8189af"))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(objed-cursor-color "#f7768e")
+ '(pdf-view-midnight-colors (cons "#a9b1d6" "#1a1b26"))
+ '(rustic-ansi-faces
+   ["#1a1b26" "#f7768e" "#73daca" "#e0af68" "#7aa2f7" "#bb9af7" "#b4f9f8" "#a9b1d6"])
+ '(vc-annotate-background "#1a1b26")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#73daca")
+    (cons 40 "#97cba9")
+    (cons 60 "#bbbd88")
+    (cons 80 "#e0af68")
+    (cons 100 "#eaa966")
+    (cons 120 "#f4a365")
+    (cons 140 "#ff9e64")
+    (cons 160 "#e89c94")
+    (cons 180 "#d19bc5")
+    (cons 200 "#bb9af7")
+    (cons 220 "#cf8ed4")
+    (cons 240 "#e382b1")
+    (cons 260 "#f7768e")
+    (cons 280 "#d97a96")
+    (cons 300 "#bc7f9e")
+    (cons 320 "#9e84a6")
+    (cons 340 "#9099c0")
+    (cons 360 "#9099c0")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
